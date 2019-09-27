24a7c83b273bf2810a7febaa26ecbdc4b9b53d33
chaoszeroomega <2linescrossed@gmail.com>
2019-09-27 19:37:17 +1000

    Finished the basic scene transition mechanics and image hiding

--- Assets/Scripts/Scenetransition.cs
+++ Assets/Scripts/Scenetransition.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 /// <summary>
 /// This is meant to make the game change scenes - from starting on the main screen to the singleplayer or multiplayer
 /// game mode. This happens when the player presses a corresponding button. 
@@ -10,7 +11,7 @@ public class Scenetransition : MonoBehaviour
 {
     public Animator sceneSition;
     public string sceneName;
-    
+    public Image image;
     // Start is called before the first frame update
     void Start()
     {
@@ -32,13 +33,14 @@ public class Scenetransition : MonoBehaviour
         }
         //The next line here is meant to disable the image component of the Canvas so that UI buttons can be clicked in future. 
         if (this.sceneSition.GetCurrentAnimatorStateInfo(0).IsName("idlesition")){
-            
+            image.enabled = !image.enabled;
         }
         
     }
     //Note to self: Implement a swappable value for the scene name, so that it can work with both the scenes present. 
     IEnumerator LoadScene()
     {
+        image.enabled = true;
         sceneSition.SetTrigger("end");
         yield return new WaitForSeconds(1.5f);
         SceneManager.LoadScene(sceneName);
