9bc94aabd916519ea3a1cb3d5fcaf3f817bbc82d
chaoszeroomega <2linescrossed@gmail.com>
2019-09-27 19:14:19 +1000

    Established working scene transition system.

--- Assets/Scripts/Scenetransition.cs
+++ Assets/Scripts/Scenetransition.cs
@@ -1,12 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 /// <summary>
 /// This is meant to make the game change scenes - from starting on the main screen to the singleplayer or multiplayer
 /// game mode. This happens when the player presses a corresponding button. 
 /// </summary>
 public class Scenetransition : MonoBehaviour
 {
+    public Animator sceneSition;
+    public string sceneName;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -16,6 +20,29 @@ public class Scenetransition : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            sceneName = "MainGame";
+            StartCoroutine(LoadScene());
+        }
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            sceneName = "Title Screen";
+            StartCoroutine(LoadScene());
+        }
+        //The next line here is meant to disable the image component of the Canvas so that UI buttons can be clicked in future. 
+        if (this.sceneSition.GetCurrentAnimatorStateInfo(0).IsName("idlesition")){
+            
+        }
         
+    }
+    //Note to self: Implement a swappable value for the scene name, so that it can work with both the scenes present. 
+    IEnumerator LoadScene()
+    {
+        sceneSition.SetTrigger("end");
+        yield return new WaitForSeconds(1.5f);
+        SceneManager.LoadScene(sceneName);
+        sceneSition.ResetTrigger("end");
+
     }
 }
