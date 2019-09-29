676fec21e919c88f615ebc3a1038ccc0c02dc908
chaoszeroomega <2linescrossed@gmail.com>
2019-09-29 18:53:34 +1000

    Changed cursor into proper reticle design

---
+++ Assets/Scripts/CursorMover.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorMover : MonoBehaviour
+{
+    [SerializeField] private Texture2D cursorTexture;
+    private Vector2 cursorHotspot;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cursorHotspot = new Vector2(cursorTexture.height / 2f, cursorTexture.width / 2f);
+        Cursor.SetCursor(cursorTexture, cursorHotspot, CursorMode.Auto);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
