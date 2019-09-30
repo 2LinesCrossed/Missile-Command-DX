866632c06f1e71cdd4ffefb74b53f9e31a5eb1b9
chaoszeroomega <2linescrossed@gmail.com>
2019-09-30 13:17:28 +1000

    Initialised basic missile spawning

--- Assets/Scripts/CursorMover.cs
+++ Assets/Scripts/CursorMover.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class CursorMover : MonoBehaviour
 {
     [SerializeField] private Texture2D cursorTexture;
+    
+    [SerializeField] GameObject missilePrefab;
+    [SerializeField] GameObject missileLauncherPrefab;
     private Vector2 cursorHotspot;
     // Start is called before the first frame update
     void Start()
@@ -16,6 +19,10 @@ public class CursorMover : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            //The missile spawning code. Change this later to rotate the missile properly and for it to only spawn missiles from the closest silo.
+            Object.Instantiate(missilePrefab, missileLauncherPrefab.transform.position,Quaternion.identity); 
+        }
     }
 }
--- Assets/Scripts/PlayerMissileController.cs
+++ Assets/Scripts/PlayerMissileController.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class PlayerMissileController : MonoBehaviour
 {
+    private Vector2 target;
+    [SerializeField] private float speed = 5;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position = Vector2.MoveTowards(transform.position, target, speed * Time.deltaTime);
     }
 }
