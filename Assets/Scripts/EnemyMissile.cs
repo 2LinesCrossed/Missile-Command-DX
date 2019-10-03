53b20061f66150da9092ed27dac68aac02d81359
chaoszeroomega <2linescrossed@gmail.com>
2019-10-03 22:23:23 +1000

    Missile aiming functionality has been added

---
+++ Assets/Scripts/EnemyMissile.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMissile : MonoBehaviour
+{
+    [SerializeField] private float speed = 5f;
+    GameObject[] defenders;
+    Transform target = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+        defenders = GameObject.FindGameObjectsWithTag("Defenders");
+        target = defenders[Random.Range(0, defenders.Length)].transform; //This gets all of the defenders, setting them as potential attackable objects
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+    }
+}
